7d51f7af6b9247036362ca68eb11118341f06451
Ahmed abdulrazzaq <aabdulra19@bw.edu>
2021-08-27 00:36:36 -0400

    simple API project fixed

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,7 +1,7 @@
-const logger = (req,res, next) => 
-{
-    console.log("Morgan works!!!");
-    next();
-}
+// const logger = (req,res, next) => 
+// {
+//     console.log("Morgan works!!!");
+//     next();
+// }
 
-module.exports = logger;
+// module.exports = logger;
